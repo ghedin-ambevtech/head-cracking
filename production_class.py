33677d6341eab2ec1321f9f434b6e324e915b691
Alison J. Ghedin <alison.ghedin@ambevtech.com.br>
2022-07-27 17:56:13 -0300

    add first files

---
+++ production_class.py
@@ -0,0 +1,21 @@
+#     """
+#     Classe teste
+    
+#     """
+    
+    
+# class ProductionClass:
+#     """_summary_
+#     """
+#     def __init__(self) -> None:
+#         pass
+
+#     @staticmethod
+#     def method():
+#         pass
+
+
+lista = ['a', 'b', 'c']
+
+texto = ', '.join(lista).upper()
+print(texto)
